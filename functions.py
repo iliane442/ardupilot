76fcbbeef16a8cb5ca8af525e6546ae6c1d2cfa9
Iliane.Blanc <lateamshadow123@gmail.com>
2026-03-10 14:22:18 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,87 @@
+from dronekit import VehicleMode, connect
+import subprocess
+import time
+import os
+import socket
+
+def set_mode(vehicle, mode_name):
+    # Liste complète des modes pour ArduPilot Plane
+    modes_arduplane = ["MANUAL", "CIRCLE","STABILIZE","TRAINING","ACRO","FBWA","FBWB","CRUISE","AUTOTUNE","LOITER","RTL","AUTO","GUIDED","INITIALISING","QSTABILIZE","QHOVER","QLOITER","QLAND","QRTL","QAUTOTUNE","QACRO","TAKEOFF"]
+    if mode_name not in modes_arduplane:
+        print("\nmode not in list")
+        return False
+    else:
+        vehicle.mode = VehicleMode(mode_name)
+        print(f"mode changé pour {mode_name}")
+        return True
+
+
+def nettoyage():
+   print("nettoyage des scripts avant lancement")
+   os.system('pkill -9 -f "ardu|mav|sim_vehicle"')
+   return True
+
+def close(v):
+	v.close()
+	print("fermeture terminée")
+	return True
+
+def connection_vehicle():
+	connection_string = 'tcp:127.0.0.1:5760'
+	vehicle = connect(connection_string, wait_ready = True, heartbeat_timeout=60)
+	print("connection terminée")
+	return vehicle
+
+def lancement_sitl():
+	## lancement du SITL
+	ardupilot_dir = "ardupilot"  # dossier ArduPilot
+	vehicle_type = "ArduPlane"
+	frame = "plane"
+	sitl_cmd = [
+	"./Tools/autotest/sim_vehicle.py",
+	"-v",vehicle_type,
+	"-f",frame,
+	"--console",
+	#"--no-mavproxy",
+        "--map"
+    	]
+	# Lancer SITL en arrière-plan
+	print("🚀 Lancement de SITL...")
+	sitl_proc = subprocess.Popen(sitl_cmd, cwd=ardupilot_dir)
+
+	# Petite pause pour laisser SITL démarrer
+	#print("pause pour démarrer sitl")
+	time.sleep(15)  # 10 secondes, ajuste si nécessaire
+	print("-----------------------------------------------------lancement fini-----------------------------------------------------")
+	add_output()  # Ajouter les sorties MAVLink après le lancement de SITL
+	return True
+
+def add_output():
+	output_add1=('127.0.0.1', 14550)  #ajout d'une sortie pour la connection missionplaner et mavlink
+	output_add2=('127.0.0.1', 14551)  #ajout d'une sortie pour connection a missionplaner
+	udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print(f"Sortie MAVLink activée sur {output_add1[0]}:{output_add1[1]}")
+	print(f"Sortie MAVLink activée sur {output_add2[0]}:{output_add2[1]}")
+	return True
+
+def atterissage():
+	
+	return True
+
+def vol_pallier_stabilise():
+	return True
+
+def acceleration_deceleration():
+	return True
+
+def virage_x():	
+	return True
+
+def changement_altitude():
+	return True
+
+def decollage():
+	
+	print("décollage en cours")
+	return True
+
---
+++ interface.py
@@ -0,0 +1,129 @@
+import customtkinter as ctk
+import tkinter as tk
+
+
+from functions import nettoyage,close,connection_vehicle,lancement_sitl
+
+number = 0
+global dico
+dico={}
+def bouton_test():
+    print("test")
+
+
+def afficher_page2():
+    frame_page1.pack_forget()
+    frame_page2.pack(expand=True, fill="both")
+
+def afficher_page1():
+    frame_page2.pack_forget()
+    frame_page1.pack(expand=True, fill="both")
+
+def affichage_liste_maneuvres():
+    colonnes, lignes = frame_page2.grid_size()
+    print(f"La grille fait {colonnes} colonnes et {lignes} lignes.")
+    for el in dico.keys():
+        dico[el][1].grid(row=(el), column=0, sticky='w', pady=10)
+        
+def action_selection(choix):
+    global number	
+    dico[number] = [choix]
+    item = ctk.CTkLabel(frame_page2, text=f" {number}:{dico[number][0]}", font=("Arial", 12), text_color="green")
+    dico[number].append(item)
+    affichage_liste_maneuvres()
+    number += 1
+
+def suppression_maneuvre():
+      global dico
+      global number
+      try:
+        assert entree.get().isdigit(), "Veuillez entrer un nombre entier valide."
+        assert int(entree.get()) in dico.keys(), "Aucune manœuvre correspondante à ce numéro."
+        vari = int(entree.get())
+        if vari=="":
+                item = ctk.CTkLabel(frame_page2, text=f"aucune maneuvre sélectionnée", font=("Arial", 12), text_color="red")
+                item.grid(row=2, column=1, pady=10)
+        elif vari in dico.keys():
+            #print(dico[vari])
+            dico[vari][1].destroy()  # Supprimer le widget associé à la manœuvre
+            del dico[vari]
+            dico, number = indexage(dico)  # Réindexer le dictionnaire après suppression
+            affichage_liste_maneuvres()  # Réafficher la liste des manœuvres
+      except AssertionError as e:
+        item = ctk.CTkLabel(frame_page2, text=str(e), font=("Arial", 12), text_color="red")
+        item.grid(row=2, column=1, pady=10)
+        app.after(3000, item.destroy)  # Supprimer le message d'erreur après 3 secondes
+    
+
+
+def indexage(dico):
+    dico_bis={}
+    tmp=0
+    for el in dico:
+        dico_bis[tmp]=dico[el]
+        dico_bis[tmp][1].configure(text=f" {tmp}:{dico[el][0]}") # Mise à jour du texte du widget avec le nouveau numéro
+        tmp+=1
+    return dico_bis,tmp
+
+
+# 2. Création de la fenêtre principale
+ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Thèmes: "blue" (standard), "green", "dark-blue"
+app = ctk.CTk()
+app.geometry("800x800")
+app.title("Psyn")
+
+
+
+
+# --- INTERFACE 1 ---
+frame_page1 = ctk.CTkFrame(app)
+frame_page1.pack(expand=True, fill="both")
+
+label1 = ctk.CTkLabel(frame_page1, text="MENU PRINCIPAL", font=("Arial", 20))
+label1.pack(pady=20)
+
+
+frame1_bouton1 = ctk.CTkButton(frame_page1, text="nettoyage des ports", command=nettoyage, corner_radius=10)
+frame1_bouton2 = ctk.CTkButton(frame_page1, text="lancement SITL", command=lancement_sitl, corner_radius=10)
+frame1_bouton3 = ctk.CTkButton(frame_page1, text="connection du vehicule", command=connection_vehicle, corner_radius=10)
+frame1_bouton4 = ctk.CTkButton(frame_page1, text="Options ▼", command=bouton_test, corner_radius=10)
+frame1_btn_maneuvre = ctk.CTkButton(frame_page1, text="Maneuvres", command=afficher_page2)
+
+
+
+# 5. Placement des boutons sur la fenêtre
+frame1_btn_maneuvre.pack(pady=10)
+frame1_bouton1.pack(pady=10)
+frame1_bouton2.pack(pady=10)
+frame1_bouton3.pack(pady=10)
+frame1_bouton4.pack(pady=10)
+
+
+# Gestion de la page maneuvres
+
+frame_page2 = ctk.CTkFrame(app)
+
+label2 = ctk.CTkLabel(frame_page2, text="Maneuvres", font=("Arial", 20), text_color="orange")
+
+label2.place(x=200, y=20)
+
+frame2_btn_retour = ctk.CTkButton(frame_page2, text="Retour", command=afficher_page1, fg_color="gray")
+frame2_btn_retour.place(x=400, y=20)
+
+menu = ctk.CTkOptionMenu(frame_page2, 
+                         values=["décollage", "Vol en palier stabilisé", "Accélération/décélération", "Virage à x °", "changement d'altitude", "atterrissage"],
+                         command=action_selection)
+
+menu.set("ajouter une maneuvre") # Texte par défaut
+menu.place(x=400, y=50)
+entree = tk.Entry(frame_page2)
+entree.place(x=400, y=80,anchor="e")
+frame2_del_man = ctk.CTkButton(frame_page2, text="Supprimer", command=suppression_maneuvre, fg_color="red")
+frame2_del_man.place(x=400, y=110,anchor="e")
+
+
+
+# 5. Placement du bouton au centre de la fenêtre
+
+app.mainloop()
