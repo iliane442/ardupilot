bea0dd19b740e52c4ecaac01818c05340ef758f1
Iliane.Blanc <lateamshadow123@gmail.com>
2026-03-10 16:30:29 +0100

    ajustement pour que ca marche

--- functions.py
+++ functions.py
@@ -78,53 +78,11 @@ def connection_vehicle2():
 def lancement_sitl():
 	## lancement du SITL
 	ardupilot_dir = "ardupilot"  # dossier ArduPilot
-	vehicle_type = "ArduPlane"
-	frame = "plane"
-	sitl_cmd = [
-	"./Tools/autotest/sim_vehicle.py",
-	"-v",vehicle_type,
-	"-f",frame,
-	"--console",
-	#"--no-mavproxy",
-        "--map"
-    	]
 	# Lancer SITL en arrière-plan
 	print("🚀 Lancement de SITL...")
-	sitl_proc = subprocess.Popen(sitl_cmd, cwd=ardupilot_dir)
-
+	sitl_proc = subprocess.Popen(["python3", "./Tools/autotest/sim_vehicle.py", "-v", "ArduPlane", "--out", "udp:127.0.0.1:14550", "--out", "udp:127.0.0.1:14551"], cwd=ardupilot_dir)
 	# Petite pause pour laisser SITL démarrer
 	#print("pause pour démarrer sitl")
 	time.sleep(10)  # 10 secondes, ajuste si nécessaire
 	print("-----------------------------------------------------lancement fini-----------------------------------------------------")
-	add_output()  # Ajouter les sorties MAVLink après le lancement de SITL
 	return True
-
-def add_output():
-	output_add1=('127.0.0.1', 14550)  #ajout d'une sortie pour la connection missionplaner et mavlink
-	output_add2=('127.0.0.1', 14551)  #ajout d'une sortie pour connection a missionplaner
-	udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print(f"Sortie MAVLink activée sur {output_add1[0]}:{output_add1[1]}")
-	print(f"Sortie MAVLink activée sur {output_add2[0]}:{output_add2[1]}")
-	return True
-
-def atterissage():
-	
-	return True
-
-def vol_pallier_stabilise():
-	return True
-
-def acceleration_deceleration():
-	return True
-
-def virage_x():	
-	return True
-
-def changement_altitude():
-	return True
-
-def decollage():
-	
-	print("décollage en cours")
-	return True
-
