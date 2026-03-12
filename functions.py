f197171aaa40a925f97ccbd29881341f1da4449f
paulperrouillet <paulperrouillet@gmail.com>
2026-03-12 12:01:20 +0100

    Update functions.py
    déplacement de fonction de backend vers fonction

--- functions.py
+++ functions.py
@@ -4,6 +4,8 @@ import time
 import os
 import socket
 
+#==========Armement du vehicule==========
+
 def armed(master,x):
 	# ARM
 	master.mav.command_long_send(
@@ -16,14 +18,74 @@ def armed(master,x):
 		armed_state = (msg.base_mode & 0b10000000) > 0
 
 
+#==========Controle du Mode==========
+
+def set_mode(master, mode_name):
+    if mode_name not in master.mode_mapping():   # pour checker si le mode existe
+        print(f"Erreur : Le mode '{mode_name}' n'est pas reconnu par l'avion.")
+        return False
+
+    mode_id = master.mode_mapping()[mode_name] # on recupere l'identifiant parmis tous les modes
+
+    master.mav.set_mode_send(
+        master.target_system,
+        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+
+        mode_id)
+
+#==========Controle d'attitude==========
+
+def send_altitude(master,roll, pitch, yaw, thrust):
+
+	roll_rad  = radians(roll)
+	pitch_rad = radians(pitch)
+	yaw_rad   = radians(yaw)
+
+	q = euler2quat(roll_rad, pitch_rad, yaw_rad)
+	master.mav.set_attitude_target_send(
+        0,
+        master.target_system,
+        master.target_component,
+        0b00000111,
+        q,
+        0,0,0,
+        thrust
+    )
+
+#==========Lecture Attitude==========
+
+def get_attitude(master):
+	msg_ang = master.recv_match(type='ATTITUDE', blocking=True)
+	yaw = degrees(msg_ang.yaw)
+	roll=degrees(msg_ang.roll)
+	pitch=degrees(msg_ang.pitch)
+
+	msg_pos = master.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+	altitude = msg_pos.relative_alt / 1000
+
+	msg_vit = master.recv_match(type='VFR_HUD', blocking=True)
+	vit=msg_vit.groundspeed	
+
+	return {
+        "yaw": yaw,
+        "roll": roll,
+        "pitch": pitch,
+        "altitude": altitude,
+        "vitesse": vit
+    }
+
+#==========Lecture Mode==========
 
 def read_mode(master):
+
 	msg = master.recv_match(type='HEARTBEAT', blocking=True) # Récupérer le dernier message HEARTBEAT
 	mode_id = msg.custom_mode # Récupère l'id du mode actuel 
 	modes=master.mode_mapping() # Dresse le dictionnaire des modes possibles 
 	mode_name= [name for name, id in modes.items() if id == mode_id][0]# Cherche la correspondance entre l'id reçu et le mode dans le dictionnaire des modes
 	print(f"Mode actuel, Id : {mode_name},{mode_id}") 
 	return [mode_name,mode_id]
+
+#==========Modification de paramètres==========
 
 def set_param(master,name, value):
 	print(f"Envoi de {name} = {value}")
