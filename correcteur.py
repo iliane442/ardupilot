a3338808e1d1d40e330eb4edd52ca6596739191b
paulperrouillet <paulperrouillet@gmail.com>
2026-03-17 15:23:08 +0100

    Update correcteur.py
    modification du correcteur d'altitude et ajout de commentaire de clarification

--- correcteur.py
+++ correcteur.py
@@ -1,8 +1,10 @@
 from functions import get_attitude
 
+#==========Correcteur d'altitude==========
 
 def alt(master, alt_target, erreur_cum=0, dt=0.05, corr_thrust=True,thrust=0.5):
 	"""Retourne les valeurs de thrust et pitch pour stabiliser l'altitude. 
+		Correcteur PD pour le tangage et PI pour la poussée 	
 		Si l'option de correction de poussé est désactivée la correction d'erreur sera plus brutale
     	- alt_target : altitude objectif de la stabilisation
       	- thrust : valeur initiale ou précédente
@@ -14,19 +16,23 @@ def alt(master, alt_target, erreur_cum=0, dt=0.05, corr_thrust=True,thrust=0.5):
 	altitude = get_attitude(master)["altitude"]
 	erreur = alt_target - altitude
 
-# proportionnel
-	pitch = erreur * 5
+# Correcteur proportionnel dérivé
+	pitch_prop = erreur*1
+	pitch_der = derreur*0.5
+	pitch = pitch_prop+pitch_der
 
 # saturation
 	pitch = max(-10, min(pitch, 10))
+
+# Activation du correcteur pour la puissance moteur + correcteur PI
 	if corr_thrust:
 		if abs(erreur) < 3:
 			erreur_cum += erreur * dt
 
 		thrust = 0.5 + erreur_cum * 0.01
-		thrust = max(0.3, min(thrust, 1))
+		thrust = max(0.3, min(thrust, 1)) #Saturation min pour éviter le décrochage et max pour rester dans les plages de valeurs admissibles
 		
-	stabilite = abs(erreur) < 1
+	stabilite = abs(erreur) < 1 
 
 	return {
         "thrust": thrust,
@@ -36,11 +42,13 @@ def alt(master, alt_target, erreur_cum=0, dt=0.05, corr_thrust=True,thrust=0.5):
         "dt": dt
     }
 
+#==========Correcteur de vitesse==========
 
 def vit(master, vit_target, erreur_cum=0, dt=0.05):
 	'''Change la poussée pour atteindre la vitesse cible
 	-vit_target : vitesse objectif 
-	-erreur_cum : erreur intégrée sur le temps ou l'erreur à un écart de 5 m/s  
+	-erreur_cum : erreur intégrée sur le temps ou l'erreur à un écart de 5 m/s 
+	-vérification de l'oscillation de la poussé dans ce cas de figure non effectué
   '''
 
 	vitesse = get_attitude(master)["speed"]
@@ -62,7 +70,12 @@ def vit(master, vit_target, erreur_cum=0, dt=0.05):
         "dt": dt
     }
 
+#==========Correcteur de cap==========
+
 def cap(master, cap_target):
+	'''Modifie le cap de l'appareil en effectuant des virages
+	-cap_target : cap objectif à maintenir ou atteindre (techniquement couplé à la fonction send_attitude peut effectuer un virage)
+	'''
 
 	cap = get_attitude(master)["yaw"]
 
